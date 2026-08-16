72a62217fd676e2ef0a8fcf7fbafbdb1627f4005
Abhinandh S <abhinandhsuby@proton.me>
2026-08-17 00:58:58 +0530

    Update sidebar.js

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -9,7 +9,6 @@ document.addEventListener("DOMContentLoaded", () => {
   sidebar.addEventListener("click", (event) => {
     // Check if the clicked element is an <a> tag (or inside one)
     if (event.target.closest("a")) {
-      // Remove the class to hide the sidebar on all devices
       document.body.classList.remove("sidebar-toggled");
     }
   });
